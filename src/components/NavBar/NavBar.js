05041afb39858e78a90bc8cb395c4d0e2096b139
Amy Hudspith <amyhudspith999@gmail.com>
2020-06-20 22:12:53 +0100

    Changed navbar brand to DU-WIT

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">DU-WIT</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                         aria-label="Toggle navigation">
