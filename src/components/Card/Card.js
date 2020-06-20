1742930dac2c7993a922a927866399103d4028ad
moll_ha <molly.hayward@durham.ac.uk>
2020-06-20 20:34:37 +0100

    added arrow to see more

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 const Card = ({title, excerpt, img_path, link}) => (
     <div className="card shadow">
@@ -14,6 +17,7 @@ const Card = ({title, excerpt, img_path, link}) => (
                     exact
                 >
                     See More
+                    <FontAwesomeIcon className="ml-2" icon={faAngleDoubleRight}></FontAwesomeIcon>
                 </NavLink>
             </div>
     </div>
