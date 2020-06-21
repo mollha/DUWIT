3c576f7728f8ce0244e16b4ebb7f2ad4bdd1e4bf
Amy Hudspith <amyhudspith999@gmail.com>
2020-06-21 11:42:56 +0100

    Fixed my navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-      
+                <a className="navbar-brand" href="/"><img src={navbarBrandImg} style={{height: 40 + 'px'}} alt="DUWIT"/></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                         aria-label="Toggle navigation">
